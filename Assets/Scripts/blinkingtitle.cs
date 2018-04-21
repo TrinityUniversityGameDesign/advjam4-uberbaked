3dd4e74fd49554a85e7c900ef5c51a941e261075
Samantha Ortiz <samiortiz0627@yahoo.com>
2018-04-21 12:52:50 -0500

    intro scene

---
+++ Assets/Scripts/blinkingtitle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blinkingtitle : MonoBehaviour
+{
+
+    float counter = 10f;
+    Text play;
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        play = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter -= Time.deltaTime * speed;
+        Debug.Log(counter);
+        if (counter <= 5)
+        {
+            play.enabled = false;
+        }
+        else if (counter >= 5)
+        {
+            play.enabled = true;
+        }
+        if (counter < 0f)
+        {
+            counter = 10f;
+        }
+    }
+}
