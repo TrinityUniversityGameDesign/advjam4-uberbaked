b2b4da1716fac2fffde97408160fbc8997c7de65
Samantha Ortiz <samiortiz0627@yahoo.com>
2018-04-21 16:29:48 -0500

    finished

---
+++ Assets/Scripts/LoadSecondLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadSecondLevel : MonoBehaviour {
+    public string buttonStart = "Start";
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(buttonStart))
+        {
+            SceneManager.LoadScene("SecondLevel");
+        }
+    }
+}
